36c3c9b3244f72819930fda76e4c190e7e0629e9
DanielTruscaV1 <danieltrusca2008@gmail.com>
2024-09-04 01:40:03 +0300

    Small update.

--- clothify2/src/components/Header.jsx
+++ clothify2/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import styles from "../styles/Header.module.css"
 
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root'); // Set the app element for accessibility (important for screen readers)
+
 const Header = () => {
-    //
+    const [showSearchModal, setShowSearchModal] = useState(false);
     const navigate = useNavigate();
 
   return ( 
@@ -12,6 +16,7 @@ const Header = () => {
             <img
                 src="/logo2.png" 
                 className={styles.logo}
+                onClick={() => navigate('/home')}
             />
             <div style={{
                 float: "right",
@@ -19,11 +24,12 @@ const Header = () => {
                 flexDirection: "row",
             }}>
                 <img 
-                    src="/assets/icon_search1.png" 
+                    src="/icon_search1.png" 
                     className={styles.icon}
+                    onClick={() => setShowSearchModal(true)}
                 />   
                 <img 
-                    src="/assets/icon_cart1.png" 
+                    src="/icon_cart1.png" 
                     className={styles.icon}
                 />     
                 <div 
@@ -32,6 +38,43 @@ const Header = () => {
                 >
                 </div> 
             </div>
+            <Modal
+                isOpen={showSearchModal}
+                onRequestClose={() => setShowSearchModal(false)}
+                style={{
+                    content: {
+                        width: '70vw',
+                        height: "18vh",
+                        margin: 'auto',
+                        textAlign: 'center',
+                        borderRadius: '10px',
+                    },
+                    overlay: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    },
+                }}
+            >
+                <div className={styles.search}>
+                    <div style={{display: "flex", flexDirection: "row"}}>
+                        <img 
+                            src="/icon_search1.png" 
+                            className={styles.icon}
+                        />   
+                        <h1 style={{fontSize: "2.5vh", paddingTop: "1vh"}}>
+                            Search for an item
+                        </h1>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "row"}}>
+                        <input type="text"/>
+                        <button>
+                            <img 
+                                src="/ok2.png" 
+                                className={styles.icon}
+                            />  
+                        </button>
+                    </div>
+                </div>
+            </Modal>
         </div>
     </div>
   )
