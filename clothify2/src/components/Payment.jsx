f39f8b1fa38a5c79df3acd91821a08d5bc28aa5c
DanielTruscaV1 <danieltrusca2008@gmail.com>
2024-09-06 16:20:56 +0300

    Small update.

--- clothify2/src/components/Payment.jsx
+++ clothify2/src/components/Payment.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
-import "../styles/Payment.module.css";
+import styles from "../styles/Payment.module.css"; // Import the CSS module
 
 const ProductDisplay = () => (
   <section>
-    <div className="product">
+    <div className={styles.product}>
       <img
         src="https://imagedelivery.net/maTmUOtE_OG9P8IykvHTIA/191256ee-1920-40e8-b9c4-61b4a3bda700/public"
         alt="The cover of Stubborn Attachments"
       />
-      <div className="description">
-      <h3>Puma T-shirt</h3>
-      <h5>$24</h5>
+      <div className={styles.description}>
+        <h3>Puma T-shirt</h3>
+        <h5>$24</h5>
       </div>
     </div>
     <form action="http://localhost:4242/create-checkout-session" method="POST">
